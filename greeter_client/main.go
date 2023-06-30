32d18801b5e0b0fd71492a24365cca7c829bc22f
Dan Wolf <dcwolf@gmail.com>
2023-06-29 23:56:38 -0500

    remove deprecated token type

--- greeter_client/main.go
+++ greeter_client/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"log"
 	"os"
@@ -10,7 +12,6 @@ import (
 	"golang.org/x/oauth2"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/credentials/oauth"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
@@ -25,6 +26,24 @@ var (
 	name = flag.String("name", defaultName, "Name to greet")
 )
 
+type TokenSource struct {
+	oauth2.TokenSource
+}
+
+func (ts *TokenSource) GetRequestMetadata(ctx context.Context, in ...string) (map[string]string, error) {
+	token, err := ts.Token()
+	if err != nil {
+		return nil, err
+	}
+	return map[string]string{
+		"authorization": "Bearer " + token.AccessToken,
+	}, nil
+}
+
+func (ts *TokenSource) RequireTransportSecurity() bool {
+	return true
+}
+
 func main() {
 	flag.Parse()
 
@@ -37,17 +56,29 @@ func main() {
 	// Create a credentials object with the JWT
 	token := &oauth2.Token{
 		AccessToken: tokenString,
+		TokenType:   "Bearer",
 	}
-	creds := oauth.NewOauthAccess(token)
+	creds := &TokenSource{
+		oauth2.StaticTokenSource(token),
+	}
 
 	// Create a certificate
-	cert, err := credentials.NewClientTLSFromFile(certFile, "")
+	cert, err := os.ReadFile(certFile)
 	if err != nil {
 		log.Fatalf("could not load tls cert: %s", err)
 	}
+	cp := x509.NewCertPool()
+	if !cp.AppendCertsFromPEM(cert) {
+		log.Fatalf("failed to append certificates")
+	}
+	config := &tls.Config{
+		InsecureSkipVerify: true, // do not use in production!
+		RootCAs:            cp,
+	}
+	connCreds := credentials.NewTLS(config)
 
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(cert), grpc.WithPerRPCCredentials(creds))
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(connCreds), grpc.WithPerRPCCredentials(creds))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
